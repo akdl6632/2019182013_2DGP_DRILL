9252017d778ff090c62eac8f843ec2cd50554545
akdl6632 <akdl6632@naver.com>
2022-09-18 15:42:59 +0900

    drill 04 완성
    2019182013 김태현

---
+++ 2019182013_2DGP_DRILL04/drill4.py
@@ -0,0 +1,23 @@
+import turtle
+
+count = 1
+while count < 7:
+    turtle.forward(500)
+    turtle.penup()
+    turtle.goto(0,100*count)
+    turtle.pendown()
+    count += 1
+
+count = 1
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.left(90)
+while count < 7:
+    turtle.forward(500)
+    turtle.penup()
+    turtle.goto(100 * count,0)
+    turtle.pendown()
+    count += 1
+
+turtle.exitonclick()
