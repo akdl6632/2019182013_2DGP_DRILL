8d925979c93a26f32e12132bdf0e90e96e664d46
akdl6632 <akdl6632@naver.com>
2022-09-19 16:29:30 +0900

    drill 05 완성
    2019182013 김태현

---
+++ 2019182013_2DGP_DRILL05/drill05.py
@@ -0,0 +1,37 @@
+import turtle
+
+def up():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def down():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp() 
+
+def left():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+    
+def right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart():
+    turtle.reset()
+
+
+turtle.shape("turtle")
+
+turtle.stamp()
+
+turtle.onkey(up,'w')
+turtle.onkey(left,'a')
+turtle.onkey(down,'s')
+turtle.onkey(right,'d')
+turtle.onkey(restart,'Escape')
+
+turtle.listen()
