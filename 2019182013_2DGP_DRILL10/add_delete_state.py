2ae908d4a59cf433c9c1d2495fdc3a5b31b5a7a8
TIP219-29 <akdl6632@naver.com>
2022-10-17 13:19:20 +0900

    drill 10 완성
    2019182013 김태현

---
+++ 2019182013_2DGP_DRILL10/add_delete_state.py
@@ -0,0 +1,55 @@
+import pico2d
+
+import game_framework
+import play_state
+import title_state
+from pico2d import *
+
+# running = True
+image = None
+logo_time = 0.0
+num = 1
+
+def enter():
+    global image
+    image = load_image('add_delete_boy.png')
+    pass
+
+
+def exit():
+    global image
+    del image
+
+
+def update():
+    play_state.update()
+    pass
+
+
+def draw():
+    clear_canvas()
+    play_state.draw_world()
+    image.draw(400, 300)
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            match event.key:
+                case pico2d.SDLK_ESCAPE:
+                    game_framework.pop_state() # 이전 상태인 play_state로 돌아감
+                case pico2d.SDLK_k:
+                    play_state.team += [play_state.Boy()]
+                    game_framework.pop_state()
+                case pico2d.SDLK_j:
+                    if len(play_state.team) > 1:
+                        del play_state.team[len(play_state.team) - 1]
+                    game_framework.pop_state()
+
+
+
+
