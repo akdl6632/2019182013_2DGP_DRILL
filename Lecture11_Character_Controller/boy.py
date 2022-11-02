79a54dbe4f8cdef8173c3bdcfc9eb777a42c7f33
TIP209 <>
2022-11-02 10:55:59 +0900

    11_02 수업내용 추가

--- Lecture11_Character_Controller/boy.py
+++ Lecture11_Character_Controller/boy.py
@@ -50,25 +50,6 @@ class AUTO_RUN:
         elif self.dir == 1:
             self.image.clip_draw(self.frame * 100, 100, 100, 100, self.x, self. y + 30, 200, 200)
 
-class SLEEP:
-
-    def enter(self, event):
-        print('ENTER SLEEP')
-        self.frame = 0
-
-    def exit(self):
-        pass
-    def do(self):
-        self.frame = (self.frame + 1) % 8
-
-    def draw(self):
-        print('DRAW SLEEP')
-        if self.face_dir == -1:
-            self.image.clip_composite_draw(self.frame * 100, 200, 100, 100,
-                                 -3.141592 / 2, '', self.x + 25, self.y - 25, 100, 100)
-        else:
-            self.image.clip_composite_draw(self.frame * 100, 300, 100, 100,
-                                 3.141592 / 2, '', self.x - 25, self.y - 25, 100, 100)
 class IDLE:
     def enter(self, event): # 상태에 들어갈 때 행하는 액션
         print('ENTER IDLE')
@@ -80,7 +61,8 @@ class IDLE:
         self.frame = (self.frame + 1) % 8
         self.timer -= 1
         if self.timer == 0:
-            self.add_event(TIMER)
+            # self.q.insert(0, TIMER)
+            self.add_event(TIMER) # 조금 더 객체지향적인 방법
     def draw(self):
         if self.face_dir == 1:
             self.image.clip_draw(self.frame * 100, 300, 100, 100, self.x, self.y)
@@ -91,6 +73,9 @@ class RUN:
     @staticmethod # 이 함수는 객체용이 아니라 클래스 용임을 알려주는 말
     def enter(self, event):
         print('ENTER RUN')
+        # 방향을 결정해야 하는데, 뭘 근거로? 어떤 키가 눌렸기 때문에
+        # 키 이벤트 정보가 필요.
+
         if event == RD:
             self.dir += 1
         elif event == LD:
@@ -108,6 +93,7 @@ class RUN:
     @staticmethod
     def do(self):
         self.frame = (self.frame + 1) % 8
+        # x 좌표 변경, 달리기
         self.x += self.dir
         self.x = clamp(0, self.x, 800)
 
@@ -119,12 +105,46 @@ class RUN:
         elif self.dir == 1:
             self.image.clip_draw(self.frame * 100, 100, 100, 100, self.x, self. y)
 
+class SLEEP:
+
+    def enter(self, event):
+        print('ENTER SLEEP')
+        self.frame = 0
+
+    def exit(self):
+        pass
+    def do(self):
+        self.frame = (self.frame + 1) % 8
+
+    def draw(self):
+        print('DRAW SLEEP')
+        if self.face_dir == -1:
+            self.image.clip_composite_draw(self.frame * 100, 200, 100, 100,
+                                             -3.141592 / 2, '',
+                                            self.x + 25, self.y - 25, 100, 100)
+        else: # 오른쪽 눕기
+            self.image.clip_composite_draw(self.frame * 100, 300, 100, 100,
+                                 3.141592 / 2, '', self.x - 25, self.y - 25, 100, 100)
+
+#3. 상태 변환 기술
+next_state = {
+    IDLE: {RD: RUN, LD:RUN, RU:RUN, LU:RUN, TIMER: SLEEP, AD: AUTO_RUN},
+    RUN: {RU: IDLE, LU:IDLE, RD: IDLE, LD:IDLE, AD: AUTO_RUN},
+    SLEEP: {RU: RUN, LU:RUN, RD:RUN, LD:RUN, AD:SLEEP},
+    # AUTO_RUN: {AD: IDLE, RU: RUN, LU:RUN, RD:RUN, LD:RUN}
+    AUTO_RUN: {AD: IDLE, RU: AUTO_RUN, LU:AUTO_RUN, RD:RUN, LD:RUN}
+}
+
 class Boy:
+
+
+    def add_event(self, event):
+        self.q.insert(0, event)
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
             key_event = key_event_table[(event.type, event.key)]
-            self.q.insert(0, key_event)
+            self.add_event(key_event)
         # if event.type == SDL_KEYDOWN:
         #     match event.key:
         #         case pico2d.SDLK_LEFT:
@@ -141,7 +161,7 @@ class Boy:
         #             self.face_dir = 1
 
     def __init__(self):
-        self.x, self.y = 800 // 2, 90
+        self.x, self.y = 0, 90
         self.frame = 0
         self.dir, self.face_dir = 0, 1
         self.image = load_image('animation_sheet.png')
@@ -168,24 +188,5 @@ class Boy:
 
     def draw(self):
         self.cur_state.draw(self)
-
-        # if self.dir == -1:
-        #     self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
-        # elif self.dir == 1:
-        #     self.image.clip_draw(self.frame*100, 100, 100, 100, self.x, self.y)
         # else:
-        #     if self.face_dir == 1:
-        #         self.image.clip_draw(self.frame * 100, 300, 100, 100, self.x, self.y)
-        #     else:
-        #         self.image.clip_draw(self.frame * 100, 200, 100, 100, self.x, self.y)
 
-    def add_event(self, event):
-        self.q.insert(0, event)
-
-#3. 상태 변환 기술
-next_state = {
-    IDLE: {RU: RUN, LU:RUN, RD:RUN, LD:RUN, TIMER: SLEEP, AD: AUTO_RUN},
-    RUN: {RU: IDLE, LU:IDLE, RD: IDLE, LD:IDLE, AD: AUTO_RUN},
-    SLEEP: {RU: RUN, LU:RUN, RD:RUN, LD:RUN, AD : AUTO_RUN},
-    AUTO_RUN: {AD: IDLE, RU: RUN, LU:RUN, RD:RUN, LD:RUN}
-}
