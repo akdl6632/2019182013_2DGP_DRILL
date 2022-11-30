633de73835d3dfc6a19deef4a7aeb59972c7d2c3
TIP209 <>
2022-11-30 11:13:44 +0900

    11_30 수업자료

---
+++ Lecture17_Game_Data/test_dic_update.py
@@ -0,0 +1,6 @@
+data = {'name': 'yuri', 'x': 100, 'y': 200}
+data['name'] = 'tom'
+data['x'] = 200
+data['y'] = 100
+new_data = {'name': 'jeny', 'x': 300, 'y': 400}
+data.update({'name': 'jonh', 'x': 200, 'y': 300})
---
+++ Lecture17_Game_Data/test_pickle_dump.py
@@ -0,0 +1,16 @@
+import pickle
+
+class NPC:
+    def __init__(self, name, x, y):
+        self.name, self.x, self.y = name, x, y
+
+    def show(self):
+        print(f'Name:{self.name} @ ({self.x}, {self.y})')
+
+yuri = NPC('Yuri', 100, 200)
+print(yuri.__dict__) # 모든 객체는 __dict__ 라는 객체가 존재
+
+yuri.name = 'tom'
+yuri.x = 100
+
+yuri.__dict__.update({'name': 'jeni', 'x': 100, 'y': 100})
